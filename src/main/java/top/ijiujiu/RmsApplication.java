028131890ed2bd783f31a6e39d634cf6822ccd37
pengxl <pengxljava@163.com>
2019-04-12 10:52:11 +0800

    初始化项目

---
+++ src/main/java/top/ijiujiu/RmsApplication.java
@@ -0,0 +1,13 @@
+package top.ijiujiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmsApplication.class, args);
+	}
+
+}
